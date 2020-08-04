4b01e88c18dfed4b6e335b5b983b174fe8d08518
ridvansumset <ridvansumset@gmail.com>
2020-08-04 23:55:11 +0300

    init repo

---
+++ public/main.js
@@ -0,0 +1,63 @@
+const app = new Vue({
+  el: '#app',
+  data() {
+    return {
+      endTime: 60,
+      distance: 60,
+      endDate: 0,
+      interval: '',
+      bgColor: '#fff',
+      isStopped: true,
+      userClicked: false,
+      m: 1,
+      s: 0,
+    };
+  },
+  methods: {
+    stopTimer() {
+      clearInterval(this.interval);
+      this.isStopped = true;
+    },
+    startTimer(isContinue) {
+      clearInterval(this.interval);
+      this.isStopped = false;
+      this.userClicked = true;
+      this.bgColor = '#fff';
+
+      const timeToAdd = isContinue ? Number(this.distance) : Number(this.endTime);
+      this.endDate = Math.floor(new Date().getTime() / 1000) + Math.floor(timeToAdd);
+
+      this.countdown(this.endDate);
+      this.interval = setInterval(() => this.countdown(this.endDate), 1000);
+    },
+    countdown(end) {
+      const distance = end - Math.floor(new Date().getTime() / 1000);
+      this.distance = distance;
+
+      if (distance < 0) {
+        this.m = 0;
+        this.s = 0;
+        clearInterval(this.interval);
+        this.startTimer();
+      } else {
+        if (distance < 4) {
+          if (distance > 0) {
+            this.beep();
+          }
+          this.bgColor = 'lightcoral';
+        } else {
+          this.bgColor = '#fff';
+        }
+        this.m = Math.floor(distance / 60);
+        this.s = distance % 60;
+      }
+    },
+    doubleDigits(t) {
+      return t > 9 ? t : `0${t}`;
+    },
+    beep() {
+      const audio = new Audio('beep.mp3');
+      audio.play();
+    },
+  },
+});
