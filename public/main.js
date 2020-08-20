82111d1fd4f2a89a337b35a6658f95bfbf78d3a1
ridvansumset <ridvansumset@gmail.com>
2020-08-21 00:39:45 +0100

    add rest time and interval count

--- public/main.js
+++ public/main.js
@@ -4,16 +4,20 @@ const app = new Vue({
   el: '#app',
   data() {
     return {
-      beepStyle: '2',
+      isWorking: false,
       endTime: 60,
+      restTime: 10,
       distance: 60,
       endDate: 0,
       interval: '',
+      intervalCount: 0,
       bgColor: '#fff',
+      beepStyle: '2',
       autoRefresh: true,
       showSettings: false,
       isStopped: true,
       userClicked: false,
+      audio: null,
       m: 1,
       s: 0,
     };
@@ -22,7 +26,16 @@ const app = new Vue({
     endTime() {
       this.stopTimer();
       this.userClicked = false;
-      this.checkEndTime();
+      this.bgColor = '#fff';
+      this.endTime = this.checkTime(this.endTime);
+      this.m = Math.floor(this.endTime / 60);
+      this.s = this.endTime % 60;
+    },
+    restTime() {
+      this.stopTimer();
+      this.userClicked = false;
+      this.bgColor = '#fff';
+      this.restTime = this.checkTime(this.restTime);
       this.m = Math.floor(this.endTime / 60);
       this.s = this.endTime % 60;
     },
@@ -32,14 +45,40 @@ const app = new Vue({
       clearInterval(this.interval);
       this.isStopped = true;
     },
-    startTimer(isContinue) {
+    startWorkTimer(isContinue) {
       clearInterval(this.interval);
-      this.isStopped = false;
+
+      if (this.endTime < 1) {
+        this.isStopped = true;
+        return;
+      }
+
+      this.isWorking = true;
       this.userClicked = true;
       this.bgColor = '#fff';
 
+      if (!isContinue) {
+        this.intervalCount += 1;
+      }
+
       const timeToAdd = isContinue ? Number(this.distance) : Number(this.endTime);
       this.endDate = Math.floor(new Date().getTime() / 1000) + Math.floor(timeToAdd);
+
+      this.isStopped = false;
+
+      this.countdown(this.endDate);
+      this.interval = setInterval(() => this.countdown(this.endDate), 1000);
+    },
+    startRestTimer(isContinue) {
+      clearInterval(this.interval);
+      this.isWorking = false;
+      this.userClicked = true;
+      this.bgColor = '#fff';
+
+      const timeToAdd = isContinue ? Number(this.distance) : Number(this.restTime);
+      this.endDate = Math.floor(new Date().getTime() / 1000) + Math.floor(timeToAdd);
+
+      this.isStopped = false;
 
       this.countdown(this.endDate);
       this.interval = setInterval(() => this.countdown(this.endDate), 1000);
@@ -53,18 +92,32 @@ const app = new Vue({
         this.s = 0;
         clearInterval(this.interval);
         if (this.autoRefresh) {
-          this.startTimer(false);
+          if (this.isWorking && this.restTime > 0) {
+            this.startRestTimer(false);
+          } else {
+            this.startWorkTimer(false);
+          }
         }
       } else {
-        if (distance < 4) {
-          if (this.beepStyle === '2' && distance > 0) {
-            this.beep();
-          } else if (this.beepStyle === '1' && distance === 0) {
-            this.beep();
+        if (this.isWorking) {
+          if (distance < 4) {
+            if (this.beepStyle === '2' && distance > 0) {
+              this.beep();
+            } else if (this.beepStyle === '1' && distance === 0) {
+              this.beep();
+            }
+            this.bgColor = 'lightcoral';
+          } else {
+            this.bgColor = '#fff';
           }
-          this.bgColor = 'lightcoral';
         } else {
-          this.bgColor = '#fff';
+          this.bgColor = 'olivedrab';
+          if (distance < 5) {
+            if (distance > 0) {
+              this.tick();
+            }
+            // this.bgColor = 'lightpink';
+          }
         }
         this.m = Math.floor(distance / 60);
         this.s = distance % 60;
@@ -73,17 +126,29 @@ const app = new Vue({
     doubleDigits(t) {
       return t > 9 ? t : `0${t}`;
     },
+    checkTime(t) {
+      if (t > MAX_VALUE) {
+        t = MAX_VALUE;
+      } else if (t < 1) {
+        t = 0;
+      }
+      return t;
+    },
+    onSelect(e) {
+      this.beepStyle = e.target.value;
+    },
     beep() {
       const audio = new Audio('beep.mp3');
       audio.play();
     },
-    checkEndTime() {
-      if (this.endTime > MAX_VALUE) {
-        this.endTime = MAX_VALUE;
-      }
-    },
-    onSelect(e) {
-      this.beepStyle = e.target.value;
+    tick() {
+      const audio = new Audio('tick.mp3');
+      audio.play();
+      setTimeout(() => {
+        audio.pause();
+        audio.muted = true;
+        audio.loop = false;
+      }, 1000);
     },
   },
 });
